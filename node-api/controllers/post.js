9c63e55b8d97663a1d51340c3b8ae376f84f30af
maddealer <forento@gmail.com>
2020-09-06 20:03:52 +0300

    comments

--- node-api/controllers/post.js
+++ node-api/controllers/post.js
@@ -101,7 +101,7 @@ const isPoster = (req, res, next) => {
   let isPoster = req.post && req.auth && req.post.postedBy._id == req.auth._id;
 
   if (!isPoster) {
-    return res.status(403).json({ error: "User is not authorized!" });
+    return res.status(403).json({ error: "User is not authorized!!!" });
   }
   next();
 };
